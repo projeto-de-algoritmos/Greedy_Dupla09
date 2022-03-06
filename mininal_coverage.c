b4b82dc7482f6ef57c346a6f65b240144d93831b
Gabriela Pivetta <gpivetta99@gmail.com>
2022-03-06 18:36:08 -0300

    adding minimal_coverage.c exercise

---
+++ mininal_coverage.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Pair {
+    int beginning, end;
+};
+
+struct Pair allPairs[100005];
+struct Pair solution[100005];
+
+void merge(struct Pair *v, int l, int m, int r){
+	struct Pair *c=malloc(sizeof(struct Pair)*(r-l+1));
+	int i=l,j=m+1,k=0;
+
+	while(i<=m && j<=r){
+	    if(v[i].beginning <= v[j].beginning)
+		    c[k++]=v[i++];
+    	else
+    		c[k++]=v[j++];
+  	}
+
+  	while(i<=m)
+    	c[k++]=v[i++];
+  	while(j<=r)
+      	c[k++]=v[j++];
+
+  	k=0;
+  	for(i=l;i<=r;i++)
+    	v[i]=c[k++];
+  	free(c);
+}
+
+void merge_sort(struct Pair *v, int l, int r){
+	if(l>=r)return; //criterio de parada
+	int m = (l+r)/2;
+	
+	merge_sort(v, l, m);
+	merge_sort(v, m+1, r);
+	
+	merge(v, l, m, r);
+}
+
+
+int main() {
+
+    int test_cases, M;
+    scanf("%d", &test_cases);
+
+    while(test_cases-->0) {
+
+        printf("\n");
+        scanf("%d", &M);
+
+        int numPairs = 0;
+        // get all relevant pairs in range [0,M]
+        while (scanf("%d %d", &allPairs[numPairs].beginning, &allPairs[numPairs].end),
+                allPairs[numPairs].beginning != 0 || allPairs[numPairs].end !=0){     
+            if (allPairs[numPairs].end > 0 && allPairs[numPairs].beginning < M)
+                ++numPairs;
+        }
+
+        merge_sort(allPairs, 0, numPairs-1);
+
+        // ensures a pair > M will never be selected
+        allPairs[numPairs].beginning = M + 1;
+        
+        int count = 0;
+        int currentX = 0;
+        int currentPair = 0;
+        
+        // pick the best end out of all of the possible pairs
+        while (currentX < M && currentPair < numPairs) {
+            solution[count].end = 0;
+            for (; allPairs[currentPair].beginning <= currentX; ++currentPair)
+                if (allPairs[currentPair].end > solution[count].end)
+                    solution[count] = allPairs[currentPair];
+            
+            if (solution[count].end == currentX)
+                break;
+            currentX = solution[count].end;
+            ++count;
+        }
+        
+        if (currentX >= M) {
+            printf("%d\n", count);
+            for (int i = 0; i < count; ++i)
+                printf("%d %d\n", solution[i].beginning, solution[i].end);
+        }
+        else
+            printf("0\n");
+
+    }
+
+    return 0;
+}
+
+
+// UVA - questão 10020
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=12&page=show_problem&problem=961
